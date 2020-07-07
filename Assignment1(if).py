b467770902da1265385f870734a48fad744fb83d
simplex06 <67653408+simplex06@users.noreply.github.com>
2020-07-07 13:31:19 +0200

    Add files via upload

---
+++ Assignment1(if).py
@@ -0,0 +1,16 @@
+# Write your Python codes on any IDE, push it up to your GitHub repository
+# and submit your GitHub Page link address in addition to your code as a plain text.
+# Task : Let's say you left a message in the past that prints a password you need. 
+# To see the password you wrote, you need to enter your name and the program should recognize you.
+# Write a program that 
+# 1. Takes the first name from the user and compares it to yours,
+# 2. Then if the name the user entered is the same as yours, print out such as : "Hello, Joseph! The password is : W@12",
+# 3. If the name the user entered is not the same as yours, print out such as : "Hello, Amina! See you later."
+
+name = input('Enter your name: ')
+name1 = name.capitalize()
+myname = 'Bryan' 
+if name == myname:
+    print(f"Hello, {name1}! The password is : W@12")
+else:
+    print(f"Hello, {name1}! See you later.")
